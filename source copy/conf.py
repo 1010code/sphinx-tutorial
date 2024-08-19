f318004a48a2d6efadded2630d66b02db32aead7
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-08-19 20:20:17 +0800

    modify

---
+++ source copy/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sphinx-tutorial'
+copyright = '2024, andy6804tw'
+author = 'andy6804tw'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
---
+++ source copy/test1.md
@@ -0,0 +1,34 @@
+# 我的第一個Markdown文件
+
+歡迎來到這個範例文件！這裡將展示如何在Markdown中使用不同的元素。
+
+## 嵌入YouTube影片
+
+下面是關於人工智慧的介紹影片：
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+## 使用圖片
+
+這裡是一張範例圖片，展示了人工智慧的概念：
+
+![AI Image](https://example.com/ai-image.png)
+
+## 創建清單
+
+以下是使用人工智慧的幾個主要應用領域：
+
+1. **自然語言處理** - 例如：語音助理和機器翻譯。
+2. **計算機視覺** - 例如：圖像識別和自動駕駛。
+3. **推薦系統** - 例如：電商網站的個性化推薦。
+4. **機器學習** - 例如：數據分析和預測模型。
+
+## 加入程式碼片段
+
+這裡是一個簡單的Python程式碼片段：
+
+```python
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("World"))
